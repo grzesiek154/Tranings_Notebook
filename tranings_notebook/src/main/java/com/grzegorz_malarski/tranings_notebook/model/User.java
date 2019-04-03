aaddce435df638a7d0113d45bd7a36cc7513b311
g.malarski <g.malarski@samsung.com>
2019-04-03 17:04:28 +0200

    started

---
+++ tranings_notebook/src/main/java/com/grzegorz_malarski/tranings_notebook/model/User.java
@@ -0,0 +1,16 @@
+package com.grzegorz_malarski.tranings_notebook.model;
+
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private String password;
+
+    
+}
