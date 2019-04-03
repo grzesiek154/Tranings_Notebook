2f6c266508227a1043aa6fb84284c813af82dc6b
g.malarski <g.malarski@samsung.com>
2019-04-03 16:48:19 +0200

    first commit

---
+++ tranings_notebook/src/main/java/com/grzegorz_malarski/tranings_notebook/TraningsNotebookApplication.java
@@ -0,0 +1,13 @@
+package com.grzegorz_malarski.tranings_notebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraningsNotebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraningsNotebookApplication.class, args);
+    }
+
+}
