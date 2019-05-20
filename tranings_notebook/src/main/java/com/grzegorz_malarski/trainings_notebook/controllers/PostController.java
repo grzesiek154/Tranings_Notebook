0da9cc1d7a2f2940fd33d0719dbe640c5647e3a8
grzesiek154 <g.malarski91@gmail.com>
2019-05-20 21:25:27 +0200

    controllers c.d

--- tranings_notebook/src/main/java/com/grzegorz_malarski/trainings_notebook/controllers/PostController.java
+++ tranings_notebook/src/main/java/com/grzegorz_malarski/trainings_notebook/controllers/PostController.java
@@ -4,10 +4,10 @@ import com.grzegorz_malarski.trainings_notebook.model.Post;
 import com.grzegorz_malarski.trainings_notebook.services.implementation.PostServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ValidationException;
 import java.util.Set;
 
 @RestController
@@ -26,13 +26,33 @@ public class PostController {
         if(posts.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
-            return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<>(posts, HttpStatus.OK);
         }
     }
     @GetMapping("/by_title")
     public ResponseEntity<Post> getByTitle(String title) {
         Post post = postService.findByTitle(title);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(post, HttpStatus.OK);
     }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Post> getPostNyId(@PathVariable Long id) {
+        Post post = postService.findById(id);
+
+        return new ResponseEntity<>(post, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Post> save(@RequestBody Post post, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()){
+            throw new ValidationException();
+        }
+        Post savedPost = postService.save(post);
+        return new ResponseEntity<>(savedPost, HttpStatus.OK);
+    }
+
+    //public ResponseEntity<Void> delete()
+
 }
